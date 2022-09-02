ed1631d1ee2357f42c91896176ba08809f828101
김성일 <ksi1492@foodpang.co>
2022-09-02 20:09:11 +0900

    set env of eslint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React, { FC } from 'react'
 
 const App: FC = () => {
-    return (
-        <div>
-            {'Hello, World!'}
-        </div>
-    )
+    return <div>Hello, World!</div>
 }
 
 export default App
