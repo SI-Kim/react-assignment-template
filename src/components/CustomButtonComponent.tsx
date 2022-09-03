29d16062b86730ee7f37c3d7189f98f493f29524
김성일 <ksi1492@foodpang.co>
2022-09-03 23:12:07 +0900

    update custom button layout

---
+++ src/components/CustomButtonComponent.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+
+const CustomButtonComponent: FC = () => {
+    const customButtonClassName = 'custom-button'
+    return <div className={customButtonClassName}>버튼</div>
+}
+
+export default CustomButtonComponent
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from 'react'
 import NavigationMenu from '@src/components/NavigationMenu'
 import DataTable from '@src/components/DataTable'
-
+import CustomButtonComponent from '@src/components/CustomButtonComponent'
 const Main: FC = () => {
+    const mainButtonAreaClassName = 'main-button-area'
     return (
         <main>
             <NavigationMenu />
             <DataTable />
+            {/* TODO 버튼 위치 조정  */}
+            <div className={mainButtonAreaClassName}>
+                <CustomButtonComponent />
+            </div>
         </main>
     )
 }
